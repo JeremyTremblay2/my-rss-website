dfbe9b91674181169a412566f4b318f50963752f
magranet2 <maxime.granet@etu.uca.fr>
2021-11-24 12:39:16 +0000

    Upload New File

---
+++ views/tabErreur.php
@@ -0,0 +1,5 @@
+<?php
+$PbNbLigne = "<p> Le nombre de ligne par page doit être supérieur au nombre de ligne total</p>";
+$PbChampVide = "<p> veuillez renseigner tout les champs</p>";
+
+$tabErr = [$PbNbLigne,$PbChampVide];
