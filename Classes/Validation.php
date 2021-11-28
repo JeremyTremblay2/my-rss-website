7bbd8e73c507943faabf0470919505cc11728c31
magranet2 <maxime.granet@etu.uca.fr>
2021-11-28 17:26:17 +0000

    nouvelle classe de validation pour les entrées de chaine de caractère

--- Classes/Validation.php
+++ Classes/Validation.php
@@ -10,18 +10,42 @@
 /**
  * A Validation class. Used for validates fields in a form, when a user wants to connect himself.
  */
+require_once('Constants.php');
 class Validation {
 
     static bool $errors = true;
+    public string $msg;
 
-    static function str(string $val) : string {
-        if (empty($val)) {
-            self::throwError(Constants::EMPTY_ERROR, 900);
+    static function str(string $val) : ?string {
+        try {
+            if ((string)empty($val)) {
+                throw new Exception(Constants::EMPTY_ERROR);
+            }
+            if (!is_string($val)) {
+                throw new Exception(Constants::INCORRECT_ERROR);
+            }
+            else{
+                return null; // self::cleanInput($val);
+            }
         }
-        if (!is_string($val)) {
-            self::throwError(Constants::INCORRECT_ERROR, 901);
+        catch (Exception $e) {
+            return $e->getMessage();
         }
-        return self::cleanInput($val);
+    }
+    static function entier(int $val) : ?int {
+        try {
+            if (empty($val)) {
+                self::throwError(Constants::EMPTY_ERROR, 900);
+            }
+            if (!is_int($val)) {
+                self::throwError(Constants::INCORRECT_ERROR, 901);
+            }
+            return self::cleanInput($val);
+        }
+        catch (Exception $e){
+            echo $e;
+            return null;
+        }
     }
 
     static function cleanInput(string $data): string {
@@ -30,7 +54,10 @@ class Validation {
         return htmlspecialchars($data);
     }
 
-    static function throwError($error = Constants::GLOBAL_ERROR, $errorCode = 0) {
+    /**
+     * @throws Exception
+     */
+    static function throwError($error = Constants::GLOBAL_ERROR, $errorCode = 0)  {
         if (self::$errors == true) {
             throw new Exception($error, $errorCode);
         }
