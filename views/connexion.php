f207ccee88917c2bb1cae4fc178679efdb5c4439
magranet2 <maxime.granet@etu.uca.fr>
2021-11-28 17:21:31 +0000

    maintenant quand on appel cette fonction elle retourne le message d'erreur

--- views/connexion.php
+++ views/connexion.php
@@ -1,30 +1,52 @@
-<!-- Created: 17/11/2021 by maxime.granet -->
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <link type="text/css" rel="stylesheet" href="projet.css">
-        <title>My RSS Website</title>
-    </head>
-    <body>
-        <header>
-                <img src="logo.png"/>
-                <h1>My RSS Website</h1>
-                
-                <button><a href="admin.html">admin</a></button>
-        </header>
-        
-        <section>
-            <div class="connexion">
-                <h1>Connexion</h1>
-                <form name="myForm" >
-                    <label>pseudo :</label>
-                    <input type=text pattern="[a-zA-Z0-9\d@$!%*_-#?&]+" name="name" class="field-long" placeholder="pseudo" required>
-                    <label>mot de passe :</label>
-                    <input type=password pattern="[a-zA-Z0-9]+" name="password" class="field-long" placeholder="mot de passe" required>
-                    <p><input type="submit" value="Envoyer"></p>
-                </form>
-            </div>
-        </section>
-    </body>
-</html>
+<?php
+/** Name : Connection.php
+ * Project : My RSS website
+ * Usefulness : Class allowing a user to connect himself to a database and execute some queries.
+ * Last Modification date : 18/11/2021
+ * Authors : SALVA Sébastien, VIALLEMONTEIL Sébastien
+ */
+
+/**
+ * A Connection class, allowing the user to connect himself to a database.
+ *
+ * It can execute some queries and modify data.
+ */
+class Connection extends PDO {
+    private $stmt;
+
+    /**
+     * Create a new Connection.
+     *
+     * @param string $dsn The database name.
+     * @param string $username The username of the database.
+     * @param string $password The password of the user.
+     */
+    public function __construct(string $dsn, string $username, string $password) {
+        parent::__construct($dsn, $username, $password);
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * Execute a query in the database.
+     *
+     * @param string $query The query in SQL format.
+     * @param array $parameters An array of parameters representing the types of the values to bind.
+     * @return bool Returns `true` on success, `false` otherwise.
+    */
+    public function executeQuery(string $query, array $parameters = []) : bool{
+        $this->stmt = parent::prepare($query);
+        foreach ($parameters as $name => $value) {
+            $this->stmt->bindValue($name, $value[0], $value[1]);
+        }
+        return $this->stmt->execute();
+    }
+
+    /**
+     * Get the result of the last request.
+     *
+     * @return array The results of the last request.
+     */
+    public function getResults() : array {
+        return $this->stmt->fetchall();
+    }
+}
