476d987c7e20815b8d14dfe114c54c515174fe4d
magranet2 <maxime.granet@etu.uca.fr>
2021-11-28 17:24:14 +0000

    appel Validation.php et récupère les potentiels erreurs pour les afficher

--- views/connexion.php
+++ views/connexion.php
@@ -1,52 +1,59 @@
-<?php
-/** Name : Connection.php
- * Project : My RSS website
- * Usefulness : Class allowing a user to connect himself to a database and execute some queries.
- * Last Modification date : 18/11/2021
- * Authors : SALVA Sébastien, VIALLEMONTEIL Sébastien
- */
+<!-- Created: 17/11/2021 by maxime.granet -->
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" href="css/stylesheet.css">
+        <title>My RSS Website</title>
+    </head>
+    <body>
+        <header>
+                <img src="https://imgr.search.brave.com/XcnMrUqaudF1J_X_M9oulPGbTeZ-ri9MJ_N7oXbqVWI/fit/844/225/ce/1/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/NndKcDBkTXBRcHJm/UzJOVWxELUlRSGFF/SyZwaWQ9QXBp"/>
+                <h1>My RSS Website</h1>
+                
+                <button><a href="admin.html">admin</a></button>
+        </header>
+        
+        <section>
+            <div class="connexion">
+                <h1>Connexion</h1>
+                <form name="myForm" method="post">
+                    <label>pseudo :</label>
+                    <input type=text pattern="[a-zA-Z0-9\d@$!%*_-#?&]+" name="name" class="field-long" placeholder="pseudo">
+                    <label>mot de passe :</label>
+                    <input type=password pattern="[a-zA-Z0-9]+" name="password" class="field-long" placeholder="mot de passe">
+                    <p><input type="submit" value="Envoyer"></p>
+                </form>
+                <?php
+                require("../Classes/Validation.php");
+                $e = new Validation();
+                $msg = '';
+                $deb = '<div class="error"><div class="txt">';
+                $fin = '</div></div>';
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $msg = $deb;
+                    $nb = 0;
+                    $err = $e->str($_POST["name"]);
+                    if ($err != null){
+                        $msg .= 'Nom : ';
+                        $msg .= $err;
+                        $nb = 1;
+                    }
+                    $err .= $e->str($_POST["password"]);
+                    if ($err != null){
+                        $msg .= 'Mot de passe : ';
+                        $msg .= $err;
+                        $nb = 1;
+                    }
+                    $msg .= $fin;
+                    if($nb==0){
+                        $msg='';
+                    }
+                    echo $msg;
+                }
 
-/**
- * A Connection class, allowing the user to connect himself to a database.
- *
- * It can execute some queries and modify data.
- */
-class Connection extends PDO {
-    private $stmt;
-
-    /**
-     * Create a new Connection.
-     *
-     * @param string $dsn The database name.
-     * @param string $username The username of the database.
-     * @param string $password The password of the user.
-     */
-    public function __construct(string $dsn, string $username, string $password) {
-        parent::__construct($dsn, $username, $password);
-        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-
-    /**
-     * Execute a query in the database.
-     *
-     * @param string $query The query in SQL format.
-     * @param array $parameters An array of parameters representing the types of the values to bind.
-     * @return bool Returns `true` on success, `false` otherwise.
-    */
-    public function executeQuery(string $query, array $parameters = []) : bool{
-        $this->stmt = parent::prepare($query);
-        foreach ($parameters as $name => $value) {
-            $this->stmt->bindValue($name, $value[0], $value[1]);
-        }
-        return $this->stmt->execute();
-    }
-
-    /**
-     * Get the result of the last request.
-     *
-     * @return array The results of the last request.
-     */
-    public function getResults() : array {
-        return $this->stmt->fetchall();
-    }
-}
+                ?>
+            </div>
+        </section>
+    </body>
+</html>
