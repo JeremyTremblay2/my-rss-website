d1dcb642c1d00a81a91f8cffb16da9770a040469
magranet2 <maxime.granet@etu.uca.fr>
2021-11-28 17:26:59 +0000

    Modif des valeur des constante, rajout d'un retour a la ligne après chaque erreurs

--- Classes/Constants.php
+++ Classes/Constants.php
@@ -1,7 +1,7 @@
 <?php
 
 class Constants {
-    const GLOBAL_ERROR = 'Erreur durant la validation du formulaire';
-    const INCORRECT_ERROR = 'Chaine de caractères non valide';
-    const EMPTY_ERROR = 'Champ vide';
+    const GLOBAL_ERROR = 'Erreur durant la validation du formulaire <br>';
+    const INCORRECT_ERROR = 'Chaine de caractères non valide <br>';
+    const EMPTY_ERROR = 'Veuillez renseigner ce champ <br>';
 }
