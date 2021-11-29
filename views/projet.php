2d4946eed6ab3c94df7f264a06dceac13916eed8
magranet2 <maxime.granet@etu.uca.fr>
2021-11-29 16:24:06 +0000

    Replace projet.php

--- views/projet.php
+++ views/projet.php
@@ -1,21 +1,17 @@
 <!-- Created: 17/11/2021 by maxime.granet -->
 <!DOCTYPE html>
 <html lang="fr">
-  <header>
-    <img src="logo.png"/>
-    <h1>My RSS Website</h1>
+<head>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="css/stylesheet.css">
+    <title>My RSS Website</title>
+</head>
+<header>
+    <img src="https://imgr.search.brave.com/XcnMrUqaudF1J_X_M9oulPGbTeZ-ri9MJ_N7oXbqVWI/fit/844/225/ce/1/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5y/NndKcDBkTXBRcHJm/UzJOVWxELUlRSGFF/SyZwaWQ9QXBp"/>
+    <h1><a class="h1" href="projet.php">My RSS Website</a></h1>
     
-    <button><a href="connexion.html">admin</a></button>
+    <button><a class="button" href="connexion.php">admin</a></button>
 </header>
     <body>
-        <header>
-            <h1>My RSS Website</h1>
-            <ul>
-                <li><a href="projet.html">National</a></li>
-                <li><a href="interNationnal.html">International</a></li>
-                <li><a href="connexion.html">Connection</a></li>
-            </ul>
-        </header>
-  	
     </body>
 </html>
