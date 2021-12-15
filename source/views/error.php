df1ab524f167e43362acc8b23dc14a7d40c96b48
magranet2 <maxime.granet@etu.uca.fr>
2021-12-15 14:39:09 +0100

    css de la page d'erreur remis

--- source/views/error.php
+++ source/views/error.php
@@ -8,86 +8,7 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <style>
-            body{
-                background-image: linear-gradient(to bottom,#e0cc91,lightgrey );
-                background-repeat: repeat-x;
-                min-height: 98vh;
-            }
-
-            header{
-                display: flex;
-                justify-content: space-between;
-                flex-direction: row;
-                align-items: center;
-                height: 100px;
-            }
-
-            header img{
-                width: 10%;
-            }
-
-            header h1 {
-                text-decoration: none;
-                justify-content: center;
-                color: white;
-            }
-
-            header a.button{
-                display: flex;
-                transition: 0.5s;
-                justify-content: center;
-                border:2px solid rgb(167, 89, 17);
-                border-radius: 12px;
-                height: 30px;
-                width : 10%;
-                min-width: 100px;
-                margin: 0 10px 0 0;
-                background-color: white;
-                text-decoration: none;
-                color: rgb(167, 89, 17);
-                font-size: 1rem;
-            }
-
-            header .button:hover{
-                transition: 0.5s;
-                background-color: rgb(167, 89, 17);
-                transition: 0.5s;
-                font-weight: bold;
-                color: white;
-            }
-
-            /*Vue d'erreur*/
-            main{
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                height: 90vh;
-            }
-
-            main h1{
-                color: white;
-                font-size: xxx-large;
-            }
-
-            main a.email{
-                color: black;
-                text-decoration: underline;
-            }
-
-            main h3{
-                font-size: x-large;
-            }
-
-            main .list{
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-            }
-
-        </style>
+        <link type="text/css" rel="stylesheet" href="views/css/RSS1.css">
         <meta charset="utf-8">
         <title>Erreur interne</title>
     </head>
@@ -96,7 +17,7 @@
             <img src="views/ressources/images/RSS-logo.png"/>
             <h1>My RSS Website</h1>
 
-            <a class="button" href="?action=connection">Connexion</a>
+            <a class="button" href="">Revenir en lieu sûr</a>
         </header>
         <main>
             <h1>Vous ne devriez pas être ici !!!</h1>
